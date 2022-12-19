c786ebb521f61b5e017668e2c7a44172000abe4b
afgye <96298754+afgye@users.noreply.github.com>
2022-12-19 16:18:55 +0300

    Add files via upload

--- CalculatorArabian.java
+++ CalculatorArabian.java
@@ -1,53 +1,58 @@
 package arabian;
+
+import java.awt.datatransfer.StringSelection;
 import java.util.Scanner;
-
-public class CalculatorRoman {
+public class CalculatorArabian {
     public static void main(String[] args) {
-
         Converter converter = new Converter();
-        String[] actions = {"+", "-", "/", "*"};
+        String[] action = {"+", "-", "/", "*"};
         String[] regexActions = {"\\+", "-", "/", "\\*"};
         Scanner scn = new Scanner(System.in);
         System.out.print("Input: ");
         String exp = scn.nextLine();
-
         int actionIndex=-1;
-        for (int i = 0; i < actions.length; i++) {
-            if(exp.contains(actions[i])){
+
+        for (int i = 0; true;) {
+            if(exp.contains(action[i]))
+                actionIndex = i;
+            break;
+        }for (int i = 1; i < action.length;) {
+            if (exp.contains(action[i])) {
                 actionIndex = i;
                 break;
             }
+            break;
+        }for (int i = 2; i < action.length;) {
+            if(exp.contains(action[i]))
+                actionIndex = i;
+            break;
+        }for (int i = 3; true;) {
+            if (exp.contains(action[i])) {
+                actionIndex = i;
+                break;
+            }
+            break;
         }
 
-        if(actionIndex==-1){
+        if (actionIndex==-1){
             System.out.println("throws Exception //т.к. строка не является математической операцией");
             return;
+
         }
-
-
-
         String[] data = exp.split(regexActions[actionIndex]);
 
-        if(converter.isRoman(data[0]) == converter.isRoman(data[1])){
-            int a,b;
+        if (converter.isRoman(data[0]) == converter.isRoman(data[1])) {
+            int a, b;
 
-            boolean isRoman = converter.isRoman(data[0]);
-            if(isRoman){
 
-                a = converter.romanToInt(data[0]);
-                b = converter.romanToInt(data[1]);
-
-            }else{
-
-                a = Integer.parseInt(data[0]);
-                b = Integer.parseInt(data[1]);
-            }
+            a = Integer.parseInt(data[0]);
+            b = Integer.parseInt(data[1]);
 
             int result;
-            switch (actions[actionIndex]){
+            switch(action[actionIndex]){
                 case "+":
-                    result = a+b;
-                    break;
+                result = a+b;
+                break;
                 case "-":
                     result = a-b;
                     break;
@@ -55,22 +60,19 @@ public class CalculatorRoman {
                     result = a*b;
                     break;
                 default:
+                case "/":
                     result = a/b;
                     break;
             }
-
-            if(isRoman){
-
-                System.out.println(converter.intToRoman(result));
-            }
-            else{
-
-                System.out.println("Output " + result);
-            }
+            System.out.println("Output " +result);
         }else{
             System.out.println("throws Exception //т.к. используются одновременно разные системы счисления");
         }
 
-
+        }
     }
-}
+
+
+
+
+
